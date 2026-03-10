6e45055e5c9afa786d874e354c8fa893fe9fe959
MOHAMED BOULJIHEL <med.blj93@gmail.com>
2026-03-10 14:32:19 +0000

    config base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'url'
+import { writeFileSync } from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  {
+    name: 'generate-redirects',
+    closeBundle() {
+      writeFileSync('dist/_redirects', '/*    /index.html   200\n')
+    }
+  }
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
